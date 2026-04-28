92e386b59c3da90790ab9b75a2cece59185a5a0f
Z-RR-oss <ziyadrjafellah@outlook.fr>
2026-04-28 02:34:48 +0200

    Initialisation du projet ECF

---
+++ Config/database.php
@@ -0,0 +1,15 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'vite_gourmand';
+$username = 'root';
+$password = '';
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo "Connexion OK";
+} catch (PDOException $e) {
+    echo "Erreur : " . $e->getMessage();
+}
